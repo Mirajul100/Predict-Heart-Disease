ace3d1f209cd3df059feb7397c96487ab2ce0c99
Anik <mirajulislamanik100@gmail.com>
2026-05-10 21:51:10 +0600

    deploy

--- heart_project/settings.py
+++ heart_project/settings.py
@@ -4,7 +4,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-heart-disease-prediction-secret-key-change-in-production'
 DEBUG = True
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
